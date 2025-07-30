63518a2211971fd400eecccbd1e5c8809550a27c
MKarthikeyanAI <mkarthikeyan00100010@gmail.com>
2025-07-30 23:40:00 +0530

    changed the pre audio url

--- app/(tabs)/learn/english-only.tsx
+++ app/(tabs)/learn/english-only.tsx
@@ -167,9 +167,9 @@ export default function EnglishOnlyScreen() {
 
   // Pre-generated processing audio URLs - multiple options for variety
   const PROCESSING_AUDIO_URLS = [
-    "https://dil-lms.s3.us-east-1.amazonaws.com/Pre-thinking-how-to-say-that.mp3.mp3",
+    "https://dil-lms.s3.us-east-1.amazonaws.com/pre_response_great_effort_user.mp3",
     "https://dil-lms.s3.us-east-1.amazonaws.com/Pre-lets-fix-your-sentence-gently.mp3",
-    "https://dil-lms.s3.us-east-1.amazonaws.com/Pre-give-me-a-moment.mp3"
+    "https://dil-lms.s3.us-east-1.amazonaws.com/pre_think_let_me_think_for_a_second.mp3"
   ];
   
   // Function to get a random processing audio URL
