5bee14356f957241326fc6c23c3eff20d555a406
MKarthikeyanAI <mkarthikeyan00100010@gmail.com>
2025-08-05 18:38:43 +0530

    implemented the stage 5 - exercise 3 v1

--- config/api.ts
+++ config/api.ts
@@ -7,7 +7,7 @@ if (__DEV__) {
   //added.
   // BASE_API_URL = 'https://api.dil.lms-staging.com';
 
-  BASE_API_URL = 'http://192.168.1.3:8000';
+  BASE_API_URL = 'http://10.75.57.11:8000';
 
   // BASE_API_URL = 'https://bda6-2401-4900-4df1-bc03-f029-886d-1f7c-9add.ngrok-free.app';
 } else {
