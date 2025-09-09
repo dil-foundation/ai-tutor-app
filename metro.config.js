74357e5ccba0b314a86358892ecc74aa0e369ea7
MKarthikeyanAI <mkarthikeyan00100010@gmail.com>
2025-09-09 15:13:24 +0530

    added for the android deploy

--- metro.config.js
+++ metro.config.js
@@ -11,22 +11,4 @@ config.resolver.alias = {
   '@': path.resolve(__dirname, './'),
 };
 
-// Ensure proper source map generation for production
-config.serializer.createModuleIdFactory = function () {
-  return function (path) {
-    // Use a deterministic module ID based on the path
-    return path.replace(__dirname, '').replace(/\//g, '_');
-  };
-};
-
-// Optimize for production builds
-if (process.env.NODE_ENV === 'production') {
-  config.transformer.minifierConfig = {
-    keep_fnames: true,
-    mangle: {
-      keep_fnames: true,
-    },
-  };
-}
-
 module.exports = config;
